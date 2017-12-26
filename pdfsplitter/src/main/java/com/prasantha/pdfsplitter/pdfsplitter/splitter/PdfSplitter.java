83da4e8bb11ac24feb0b13a676023f5f070db8c9
prasantha <prasanthas@gmail.com>
2017-12-26 12:25:03 +1100

    working code

--- pdfsplitter/src/main/java/com/prasantha/pdfsplitter/pdfsplitter/splitter/PdfSplitter.java
+++ pdfsplitter/src/main/java/com/prasantha/pdfsplitter/pdfsplitter/splitter/PdfSplitter.java
@@ -1,16 +1,16 @@
 package com.prasantha.pdfsplitter.pdfsplitter.splitter;
 
+import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,37 +26,98 @@ public class PdfSplitter {
         Splitter splitter = new Splitter();
         List<PDDocument> pages = splitter.split(document);
 
-        System.out.println("size: "+pages.size());
+        System.out.println("pages size: "+pages.size());
 
-        splitByStaffNo(pages);
-        
-        /*Iterator<PDDocument> iterator = pages.listIterator();
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-//Saving each page as an individual document
-        int i = 1;
-        while(iterator.hasNext()) {
-            PDDocument pd = iterator.next();
-            pd.save("output/sample"+ i++ +".pdf");
+        Map<String, List<PDDocument>> documentsMap = collectToMap(pages);
 
-            String text = pdfStripper.getText(pd);
-//            System.out.println("text: "+text);
+        printDocumentsMap(documentsMap);
+//        splitByStaffNo(pages);
 
-           Pattern p = Pattern.compile("(Staff\\s+No\\s+:\\s+)(\\d+)");
-            Matcher matcher = p.matcher(text);
 
-            if(matcher.find()) {
-//                System.out.println(text);
-                System.out.println(matcher.group(0));
-                String currentStaffNo = matcher.group(1);
-                System.out.println(currentStaffNo);
-                //check next page for the number and collatae then
-            }
-
-        }*/
         System.out.println("Multiple PDF’s created");
 
         document.close();
 
+    }
+
+    private void printDocumentsMap(Map<String, List<PDDocument>> documentsMap) {
+        System.out.println("**********START PRINTING DOCUMENT MAP ******************");
+
+        System.out.println("documents size: " + documentsMap.size());
+
+        documentsMap.forEach((k,v)->{
+            System.out.println("K: "+k);
+            System.out.println("V: "+v.size());
+
+            if (v.size() == 1) {
+                try {
+                    v.get(0).save("output/payslip_"+k+".pdf");
+                } catch (IOException e) {
+                   e.printStackTrace();
+
+                }
+            } else {
+                System.out.println("STREAM THROUGH AND CONCAT");
+                PDFMergerUtility merger = new PDFMergerUtility();
+//                merger.setDestinationFileName("output/payslip_"+k+".pdf");
+                PDDocument destination = new PDDocument(MemoryUsageSetting.setupMainMemoryOnly());
+                v.forEach((pd) -> {
+                    try {
+                        merger.appendDocument(destination,pd);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    /*
+                    PDStream pdStream = new PDStream(pd);
+                    try {
+//                        merger.addSource(new ByteArrayInputStream(pdStream.toByteArray()));
+
+                        System.out.println("Adding into addSource");
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }*/
+                });
+                /*try {
+                    System.out.println("Merging Documents");
+                    merger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                try {
+                    destination.save("output/payslip_"+k+".pdf");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        System.out.println("**********END PRINTING DOCUMENT MAP ******************");
+
+
+    }
+
+    private Map<String, List<PDDocument>> collectToMap(List<PDDocument> pages) throws IOException {
+        Pattern p = Pattern.compile("(Staff\\s+No\\s+:\\s+)(\\d+)");
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        Map<String, List<PDDocument>> documentsMap = new HashMap<String, List<PDDocument>>();
+        for (PDDocument pd: pages) {
+            String text = pdfStripper.getText(pd);
+
+            Matcher matcher = p.matcher(text);
+
+            if (matcher.find()) {
+                String staffNo = matcher.group(2);
+                if (documentsMap.containsKey(staffNo)) {
+                    documentsMap.get(staffNo).add(pd);
+                } else {
+                    ArrayList<PDDocument> pdDocuments = new ArrayList<>();
+                    pdDocuments.add(pd);
+                    documentsMap.put(staffNo,pdDocuments);
+                }
+            }
+        }
+        return documentsMap;
     }
 
     private void splitByStaffNo(List<PDDocument> pages) throws IOException {
@@ -85,7 +146,7 @@ public class PdfSplitter {
                     if (m.find()) {
                         if (!currentStaffNo.equals(m.group(2))) {
                             pd.save("output/sampleeeeeee_" + currentStaffNo+ ".pdf");
-//                            storeCollectedPdfs(collectedPdfs,previousStaffNo);
+                            storeCollectedPdfs(collectedPdfs,previousStaffNo);
                             collectedPdf=false;
                             collectedPdfs.clear();
                         }
